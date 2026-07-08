7459cf128436305833a546c89d729544bd36681b
Dung Le <letridung07@gmail.com>
2026-07-08 03:13:26 +0100

    Context Wiring: Favorite Page
    Display favorite movies in Favorite Page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,33 @@
 // Page that show favorite movies
-import "../css/Favorites.css"
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../component/MovieCard";
 
 function Favorites() {
-    return <div className="favorites-empty">
-        <h2>No Favorite Movie Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here!</p>
-    </div>
+    const { favorites } = useMovieContext();
+
+    // Display favorite movies
+    if (favorites.length > 0) {
+        return (
+            <div className="favorites">
+                <h2>Your Favorites</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorite Movie Yet</h2>
+            <p>
+                Start adding movies to your favorites and they will appear here!
+            </p>
+        </div>
+    );
 }
 
-export default Favorites
+export default Favorites;
